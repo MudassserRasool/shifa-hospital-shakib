49b393c6adc13c85ac9d147639422da0ca8b6623
mudasser <>
2024-07-01 11:11:19 +0500

    change

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
-require("dotenv").config();
-const express = require("express");
+require('dotenv').config();
+const express = require('express');
 const app = express();
-const cors = require("cors");
-const connection = require("./db");
-const authRoutes = require("./routes/auth");
-const signupRoutes = require("./routes/users");
-const patientSignup = require("./routes/patientSignup");
-const doctortiming = require("./routes/doctors");
-const appointment = require("./routes/appointment");
+const cors = require('cors');
+const connection = require('./db');
+const authRoutes = require('./routes/auth');
+const signupRoutes = require('./routes/users');
+const patientSignup = require('./routes/patientSignup');
+const doctortiming = require('./routes/doctors');
+const appointment = require('./routes/appointment');
 app.use(express.json());
 app.use(cors());
 
 connection();
-
-app.use("/api/auth", authRoutes);
-app.use("/api/signup", signupRoutes);
-app.use("/api/signup/patient", patientSignup);
-app.use("/api/add/doctortiming", doctortiming);
-app.use("/api/add/appointment", appointment);
+// run a hellow world
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+app.use('/api/auth', authRoutes);
+app.use('/api/signup', signupRoutes);
+app.use('/api/signup/patient', patientSignup);
+app.use('/api/add/doctortiming', doctortiming);
+app.use('/api/add/appointment', appointment);
 const port = process.env.PORT || 8383;
 
 app.listen(port, () => {
